f1c59506e80fce42b0ab345878592a07aaac7ed5
Matthijs de Vos <matthijs@primato.nl>
2023-09-24 16:29:14 +0200

    corrupted git folder

---
+++ components/wizardwear/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container" id="content">
+        <div class="row">
+            <div class="col text-center">
+                <h1>Welkom bij Wizard Wear</h1>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ components/wizardwear/resources/views/oom/layouts/_nav.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-md navbar-light bg-none mb-4">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav m-auto">
+                <li class="nav-item">
+                    <a href="{{ route('root') }}" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('root') }}" class="nav-link">Events</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('root') }}" class="nav-link">Foto's</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('root') }}" class="nav-link">Order of Merlin</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
