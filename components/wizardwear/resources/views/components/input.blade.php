1e25cf35026adb042929b4092e7bc4fe3c8b8f2e
Matthijs de Vos <matthijs@primato.nl>
2023-09-25 21:28:04 +0200

    work on auth form

---
+++ components/wizardwear/resources/views/components/input.blade.php
@@ -0,0 +1,14 @@
+<div class="input-group mb-3">
+    <span class="input-group-text col-3">
+        <label for="{{ $name }}" class="ms-auto">
+            {{ $label }}
+        </label>
+    </span>
+    <input
+        type="{{ $type }}"
+        id="{{ $name }}" name="{{ $name }}"
+        class="form-control @error($name) is-invalid @enderror" placeholder="{{ $attributes->get('placeholder') }}"
+        value="{{ old($name) ?? $value }}"
+        @required($attributes->has('required'))
+    >
+</div>
---
+++ components/wizardwear/resources/views/components/input/switch.blade.php
@@ -0,0 +1,8 @@
+<div class="input-group mb-3">
+    <div class="input-group-text col-3 text-right">
+        <input
+            class="form-check-input mt-0" type="checkbox"
+            value="" aria-label="Checkbox for following text input">
+    </div>
+    <input type="text" class="form-control" aria-label="Text input with checkbox">
+</div>
