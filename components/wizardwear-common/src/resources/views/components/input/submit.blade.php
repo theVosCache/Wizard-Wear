bfc5bc0f09d085aa850841fae4f95edc915958c3
Matthijs de Vos <matthijs@primato.nl>
2023-10-02 22:02:50 +0200

    init oom project

--- components/wizardwear-common/src/resources/views/components/input/submit.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="input-group mb-3">
-    <input type="submit" value="{{ $label ?? __('form.submit')}}" class="btn btn-primary form-control" />
-</div>
--- components/wizardwear-common/src/resources/views/components/input/switch.blade.php
+++
@@ -1,9 +0,0 @@
-<input type="hidden" name="{{ $name }}" value="0">
-<div class="input-group mb-3">
-    <div class="input-group-text col-3 text-right">
-        <input
-            class="form-check-input ms-auto" type="checkbox"
-            value="1" >
-    </div>
-    <input type="text" value="{{ $label }}" class="form-control disabled" disabled>
-</div>
