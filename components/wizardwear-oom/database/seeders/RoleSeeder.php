e9d8d44e9963e8568f974b3e44b063fd2ead80a2
Matthijs de Vos <matthijs@primato.nl>
2023-10-07 12:41:16 +0200

    added role model

---
+++ components/wizardwear-oom/database/seeders/RoleSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::all();
+        $admin = Role::where('slug', Role::ADMIN);
+        $board = Role::where('slug', Role::BOARD);
+
+        if (!$admin->exists()){
+            $role = new Role;
+            $role->name = ucfirst(Role::ADMIN);
+            $role->slug = Role::ADMIN;
+
+            $role->save();
+        }
+
+        if (!$board->exists()){
+            $role = new Role;
+            $role->name = ucfirst(Role::BOARD);
+            $role->slug = Role::BOARD;
+
+            $role->save();
+        }
+
+        foreach ($users as $user){
+            $user->roles()->sync([
+                $admin->first()->id, $board->first()->id
+            ]);
+        }
+    }
+}
