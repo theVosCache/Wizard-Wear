dd842030239fabce77a4948a2ce5be618453a3a7
Matthijs de Vos <matthijs@primato.nl>
2023-11-13 20:57:23 +0100

    create working monster list

--- components/wizardwear-oom/app/Livewire/Dnd/DungeonMasterScreen/MonsterList.php
+++ components/wizardwear-oom/app/Livewire/Dnd/DungeonMasterScreen/MonsterList.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Dnd\DungeonMasterScreen;
 
+use App\Http\Resources\DndCampaignMonsterData;
 use App\Models\DndCampaign;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
@@ -10,9 +11,60 @@ class MonsterList extends Component
 {
     public DndCampaign $dndCampaign;
 
+    public string $name, $monsterType;
+    public int $currentHitPoints, $totalHitPoints;
+
     public function mount(DndCampaign $dndCampaign): void
     {
         $this->dndCampaign = $dndCampaign;
+    }
+
+    public function increaseHitPoints(int $index): void
+    {
+        $data = $this->dndCampaign->data;
+
+        /** @var DndCampaignMonsterData[] $monsterList */
+        $monsterList = $data->monsterList;
+
+        if ($monsterList[$index]->currentHitPoints < $monsterList[$index]->totalHitPoints){
+            $monsterList[$index]->currentHitPoints++;
+        }
+
+        $data->monsterList = $monsterList;
+        $this->dndCampaign->data = $data;
+        $this->dndCampaign->save();
+    }
+
+    public function decreaseHitPoints(int $index): void
+    {
+        $data = $this->dndCampaign->data;
+
+        /** @var DndCampaignMonsterData[] $monsterList */
+        $monsterList = $data->monsterList;
+
+        if ($monsterList[$index]->currentHitPoints > 0){
+            $monsterList[$index]->currentHitPoints--;
+        }
+
+        $data->monsterList = $monsterList;
+        $this->dndCampaign->data = $data;
+        $this->dndCampaign->save();
+    }
+
+    public function save(): void
+    {
+        $monsterData = new DndCampaignMonsterData(
+            name: $this->name,
+            monsterType: $this->monsterType,
+            currentHitPoints: $this->currentHitPoints,
+            totalHitPoints: $this->totalHitPoints
+        );
+
+        $data = $this->dndCampaign->data;
+        $data->monsterList[] = $monsterData;
+
+        $this->dndCampaign->data = $data;
+        $this->dndCampaign->save();
     }
 
     public function render(): View
--- components/wizardwear-oom/resources/views/livewire/dnd/dungeon-master-screen/monster-list.blade.php
+++ components/wizardwear-oom/resources/views/livewire/dnd/dungeon-master-screen/monster-list.blade.php
@@ -10,30 +10,47 @@
             </thead>
 
             <tbody>
-            @foreach($dndCampaign->data->monsterList as $monster)
+            @foreach($dndCampaign->data->monsterList as $index => $monster)
                 <tr>
                     <td>
                         {{ $monster->name }} <br>
                         <span class="text-muted">{{ $monster->monsterType }}</span>
                     </td>
                     <td>
-                        <div class="text-center">
-                            {{ $monster->currentHitPoints }} / {{ $monster->totalHitPoints }}
-                        </div>
-
-                        <div class="progress h-100" role="progressbar">
-                            <div
-                                class="progress-bar {{ $monster->currentHitPointsColor() }}"
-                                style="width: {{ $monster->currentHitPointsPercentage() * 100 }}%">
-                                @if (!empty($monster->currentHitPointsPercentage()))
-                                    @if ($monster->currentHitPointsPercentage() > 0.8)
-                                        Healty
-                                    @elseif ($monster->currentHitPointsPercentage() > 0.25)
-                                        Hurt
-                                    @else
-                                        Wounded
-                                    @endif
-                                @endif
+                        <div class="row p-0">
+                            <div class="col-12 text-center">
+                                {{ $monster->currentHitPoints }} / {{ $monster->totalHitPoints }}
+                            </div>
+                            <div class="col-1 p-0">
+                                <button
+                                    class="btn btn-danger" wire:click="decreaseHitPoints({{ $index }})"
+                                    @disabled($monster->currentHitPoints === 0)
+                                >-
+                                </button>
+                            </div>
+                            <div class="col-10 px-1">
+                                <div class="progress h-100" role="progressbar">
+                                    <div
+                                        class="progress-bar {{ $monster->currentHitPointsColor() }}"
+                                        style="width: {{ $monster->currentHitPointsPercentage() * 100 }}%">
+                                        @if (!empty($monster->currentHitPointsPercentage()))
+                                            @if ($monster->currentHitPointsPercentage() > 0.8)
+                                                Healty
+                                            @elseif ($monster->currentHitPointsPercentage() > 0.25)
+                                                Hurt
+                                            @else
+                                                Wounded
+                                            @endif
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-1 p-0">
+                                <button
+                                    class="btn btn-success" wire:click="increaseHitPoints({{ $index }})"
+                                    @disabled($monster->currentHitPoints === $monster->totalHitPoints)
+                                >+
+                                </button>
                             </div>
                         </div>
                     </td>
@@ -41,5 +58,53 @@
             @endforeach
             </tbody>
         </table>
+
+        <div class="row">
+            <div class="col-12">
+                <div class="input-group mb-3">
+                    <span class="input-group-text">
+                        <label for="name" class="ms-auto">Name</label>
+                    </span>
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="name"
+                        wire:model="name"
+                    >
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text">
+                        <label for="monster_type" class="ms-auto">Monster Type</label>
+                    </span>
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="monster_type"
+                        wire:model="monsterType"
+                    >
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text">
+                        <label for="current_hit_points" class="ms-auto">Hit points</label>
+                    </span>
+                    <input
+                        type="number"
+                        class="form-control"
+                        id="current_hit_points"
+                        wire:model="currentHitPoints"
+                    >
+                    <span class="input-group-text">/</span>
+                    <input
+                        type="number"
+                        class="form-control"
+                        id="total_hit_points"
+                        wire:model="totalHitPoints"
+                    >
+                </div>
+                <div class="input-group mb-3">
+                    <input type="submit" value="Save" class="btn btn-primary form-control" wire:click="save"/>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
