f4b8fbbb032e56ed26682a02c9138286c7b03f41
Matthijs de Vos <matthijs@primato.nl>
2024-01-08 21:53:55 +0100

    join a event

--- components/wizardwear-oom/resources/views/livewire/event-join-button.blade.php
+++ components/wizardwear-oom/resources/views/livewire/event-join-button.blade.php
@@ -1,3 +1,7 @@
-<div>
-
+<div wire:poll>
+    @if($joined)
+        <button class="btn btn-success" wire:click="unJoinEvent()">Aanwezig</button>
+    @else
+        <button class="btn btn-danger" wire:click="joinEvent()">Afwezig</button>
+    @endif
 </div>
