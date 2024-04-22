ea362969026d8a36fefff516a94ae43085eafa5f
Matthijs De vos <m.devos96@gmail.com>
2024-04-22 21:09:08 +0200

    dropped block editor

--- components/wizardwear-oom/resources/views/livewire/cms/drop-block-editor/save-button.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    @if(!empty($page))
-        <button wire:click="save" class="btn btn-success">Save</button>
-    @else
-        <button class="btn btn-danger disabled" disabled>Save</button>
-    @endif
-</div>
